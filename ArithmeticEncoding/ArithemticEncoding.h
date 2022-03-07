1a8bcab6c7181399db67ea43eefe6e951b03d5e8
Andre Z <andrez.quix@gmail.com>
2022-03-07 16:59:27 +0300

    Translate comments from Russian to English

--- ArithmeticEncoding/ArithemticEncoding.h
+++ ArithmeticEncoding/ArithemticEncoding.h
@@ -6,14 +6,14 @@
 
 using namespace std;
 
-void calculateAlphabet(const string& text, string& alphabet); // рассчитать алфавит текста
+void calculateAlphabet(const string& text, string& alphabet); // calculate text alphabet
 
-void calculateProbability(const string& text, const string& alphabet, vector<double>& probabilities);	// рассчитать вероятность появления каждого символа алфавита в тексте
+void calculateProbability(const string& text, const string& alphabet, vector<double>& probabilities);	// calculate the probabillity of symbol occurrence in the text
 
-vector<segment> defineSegments(const string& str, const vector<double> probabilities);	// рассчитать интервалы сегментов, отвечающих за символы алфавита
+vector<segment> defineSegments(const string& str, const vector<double> probabilities);	// calculate the segments intervals
 
-const segment& getSegBySymb(const vector<segment>& segments, char symb); // получить интервалы сегмента по его символу
+const segment& getSegBySymb(const vector<segment>& segments, char symb); // get segment intervals by the symbol it owned
 
-double arithmeticEncoding(const string& text, const string& alphabet, vector<double>& probabilities);	// закодировать текст
+double arithmeticEncoding(const string& text, const string& alphabet, vector<double>& probabilities);	// encode text
 
-string arithmeticDecoding(const string& alphabet, const vector<double>& probabilities, double code, int textSize);	// декодировать текст
+string arithmeticDecoding(const string& alphabet, const vector<double>& probabilities, double code, int textSize);	// decode text
