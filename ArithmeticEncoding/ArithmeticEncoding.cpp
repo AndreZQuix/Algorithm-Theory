776641bcb905243ce7ad4503d95882015fa0921d
Andre Z <andrez.quix@gmail.com>
2022-03-07 16:56:26 +0300

    Translate comments from Russian to English

--- ArithmeticEncoding/ArithmeticEncoding.cpp
+++ ArithmeticEncoding/ArithmeticEncoding.cpp
@@ -3,9 +3,9 @@
 
 struct segment
 {
-    char symb;  // символ
-    double left;    // левый интервал
-    double right;   // правый интервал
+    char symb;  // symbol
+    double left;    // left interval
+    double right;   // right interval
 };
 
 vector<segment> defineSegments(const string& str, const vector<double> probabilities)
@@ -14,7 +14,7 @@ vector<segment> defineSegments(const string& str, const vector<double> probabili
     const int m = str.size();
     vector<segment> segments;
     segments.resize(m);
-    for (int i = 0; i < m; i++) // проводится расчет сегментов
+    for (int i = 0; i < m; i++) // calculate segments intervals
     {
         segments[i].symb = str[i];
         segments[i].left = l;
@@ -26,7 +26,7 @@ vector<segment> defineSegments(const string& str, const vector<double> probabili
     return segments;
 }
 
-const segment& getSegBySymb(const vector<segment>& segments, char symb) // найти интервалы сегмента по символу, которому принадлежит интервал
+const segment& getSegBySymb(const vector<segment>& segments, char symb) // find segment intervals by symbol
 {
     for (const auto& seg : segments)
     {
@@ -43,7 +43,7 @@ double arithmeticEncoding(const string& text, const string& alphabet, vector<dou
     char symb;
     double newRight, newLeft;
     cout << "--------------------" << endl;
-    for (int i = 0; i < text.size(); i++)   // процесс кодировки
+    for (int i = 0; i < text.size(); i++)   // encoding process
     {
         symb = text[i];
         cout << "Symbol: " << symb << " Segment.left: " << getSegBySymb(segments, symb).left << " Segments.right: " << getSegBySymb(segments, symb).right << '\n';
@@ -59,48 +59,47 @@ double arithmeticEncoding(const string& text, const string& alphabet, vector<dou
     return (left + right) / 2;
 }
 
-string arithmeticDecoding(const string& alphabet, const vector<double>& probabilities, double code, int textSize)   // декодирование строки
+string arithmeticDecoding(const string& alphabet, const vector<double>& probabilities, double code, int textSize)   // decoding process
 {
-    vector<segment> segments = defineSegments(alphabet, probabilities); // расчет сегментов
+    vector<segment> segments = defineSegments(alphabet, probabilities);
     string text;
-    for (int i = 0; i < textSize; i++) // декодируем символы, граница - номер последнего символа закодированного текста
+    for (int i = 0; i < textSize; i++) // string decoding
     {
-        for (int j = 0; j < alphabet.size(); j++)   // ищем сегменты, в интервалы которых входит код
-            if (code >= segments[j].left && code < segments[j].right && text.size() < textSize) // если код попал в интервал и при этом не перешел границу...
+        for (int j = 0; j < alphabet.size(); j++)   // searching segments, whose intervals include code
+            if (code >= segments[j].left && code < segments[j].right && text.size() < textSize) // if code is in interval and didn't cross the textSize border...
             {
-                text += segments[j].symb;   // ...добавить символ в строку...
-                code = (code - segments[j].left) / (segments[j].right - segments[j].left);  // ...расчитать новое значение кода для поиска в сегментах
-            }
+                text += segments[j].symb;   // ...add symbol to string...
+                code = (code - segments[j].left) / (segments[j].right - segments[j].left);  // ...calculate new code value for next searching iteration
     }
     return text;
 }
 
-void calculateAlphabet(const string& text, string& alphabet)    // рассчитать алфавит для закодирования
+void calculateAlphabet(const string& text, string& alphabet)    // calculate encoding alphabet
 {
     alphabet = text;
     sort(alphabet.begin(), alphabet.end());
-    alphabet.erase(unique(alphabet.begin(), alphabet.end()), alphabet.end());   // получаем уникальный набор символов в введенной строке (т.е. алфавит)
+    alphabet.erase(unique(alphabet.begin(), alphabet.end()), alphabet.end());   // getting unique symbols set in the string (i.e alphabet)
 }
 
-void calculateProbability(const string& text, const string& alphabet, vector<double>& probabilities)    // рассчитать вероятности появления символов алфавита в тексте
+void calculateProbability(const string& text, const string& alphabet, vector<double>& probabilities)    // caluclate probabilities of symbol occurrence in the text
 {
     double symbCount = 0;
     double size = text.size();
     for (const auto& alphabetSymb : alphabet)
     {
-        for (const auto& textSymb : text)   // увеличиваем счетчик каждый раз, когда символ алфавита встречается в тексте
+        for (const auto& textSymb : text)   // if alphabet character is in the text, increase counter value
         {
             if (alphabetSymb == textSymb)
                 symbCount++;
         }
-        probabilities.push_back(symbCount / size);  // в вектор вероятностей добавляем посчитанную вероятность, обнуляем счетчик, переходим к следующему символу алфавита
+        probabilities.push_back(symbCount / size);  // add probabillity value
         symbCount = 0;
     }
 }
 
 int main()
 {
-    cout << "Enter the string you want to encode: " << endl; // введите строку для закодирования
+    cout << "Enter the string you want to encode: " << endl;
     string text;
     getline(cin, text);
     cout << "Text size: " << text.size();
